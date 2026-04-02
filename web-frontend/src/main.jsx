1179c24de0d60bd7bf1778af29473ec92e147e86
Thuvarakesh.V <thuvarakesh27@gmail.com>
2026-04-02 22:52:37 +0530

    feat: add main application structure with routing and styles

---
+++ web-frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+import '@fontsource/manrope/700.css'
+import '@fontsource/manrope/800.css'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/600.css'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(<App />)
